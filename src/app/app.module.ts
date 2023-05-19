d26832096da01a20210eac6390ecada737fc176f
srikanth <bga.srikanth@gmail.com>
2023-05-19 16:57:32 +0530

    styles add this project into main folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,    
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
